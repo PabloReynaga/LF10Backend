a9a0e99ab23fb605c5b7edc1174e1fcb5889c67b
PabloReynaga <reynagafarias@gmail.com>
2024-12-06 23:58:48 +0100

    ADD - Add

---
+++ src/main/java/LF10/Backend/Lf10BackendApplication.java
@@ -0,0 +1,16 @@
+package LF10.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lf10BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lf10BackendApplication.class, args);
+
+		System.out.println("Hello, World!");
+	}
+
+
+}
---
+++ src/test/java/LF10/Backend/Lf10BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package LF10.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lf10BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
